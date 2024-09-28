cbebf0c21858f860ab1c0a1fa9fd30dc70c43fc2
aldiyunan07 <aldiyunan07@gmail.com>
2024-09-28 14:13:10 +0700

    add scramble documentation api

--- routes/api.php
+++ routes/api.php
@@ -9,33 +9,32 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\TransferController;
 use App\Http\Controllers\WalletController;
 
-Route::post('/login', [LoginController::class,'__invoke']);
-Route::post('/register', [registerController::class,'__invoke']);
-Route::get('/check',[CheckController::class,'__invoke']);
-Route::middleware('auth:api')->group(function(){
-  Route::post('/logout', [LogoutController::class,'__invoke']);
-  Route::get('/users',[MainController::class,'listUser']);
-  Route::get('/user/{user}',[MainController::class,'userDetail']);
-  Route::get('/me', [MainController::class,'me']);
-  Route::get('/transfer/history',[TransferController::class,'history']);
+Route::post('/login', [LoginController::class, '__invoke']);
+Route::post('/register', [registerController::class, '__invoke']);
+Route::get('/check', [CheckController::class, '__invoke']);
+Route::middleware('auth:api')->group(function () {
+  Route::post('/logout', [LogoutController::class, '__invoke']);
+  Route::get('/users', [MainController::class, 'listUser']);
+  Route::get('/user/{user}', [MainController::class, 'userDetail']);
+  Route::get('/me', [MainController::class, 'me']);
+  Route::get('/transfer/history', [TransferController::class, 'history']);
   Route::get('/transaction/history', [TransactionController::class, 'history']);
 
-  Route::prefix('wallet')->group(function(){
-    Route::get('/my-balance',[WalletController::class,'index']);
-    Route::post('/update-pin',[WalletController::class,'updatePin']);
-  });
-  
-  Route::prefix('transaction')->group(function(){
-    Route::post('/',[TransactionController::class,'store']);
-    Route::post('/withdraw',[TransactionController::class,'withdraw']);
-    Route::get('/{transaction}',[TransactionController::class,'detail']);
-    Route::post('/confirmation/{transaction}',[TransactionController::class,'confirmation']);
-
+  Route::prefix('wallet')->group(function () {
+    Route::get('/my-balance', [WalletController::class, 'index']);
+    Route::post('/update-pin', [WalletController::class, 'updatePin']);
   });
 
-  Route::prefix('transfer')->group(function(){
-    Route::post('/',[TransferController::class,'store']);
-    Route::get('/{transfer}',[TransferController::class,'detail']);
-    Route::post('/confirmation/{transfer}',[TransferController::class,'confirmation']);
+  Route::prefix('transaction')->group(function () {
+    Route::post('/', [TransactionController::class, 'store']);
+    Route::post('/withdraw', [TransactionController::class, 'withdraw']);
+    Route::get('/{transaction}', [TransactionController::class, 'detail']);
+    Route::post('/confirmation/{transaction}', [TransactionController::class, 'confirmation']);
+  });
+
+  Route::prefix('transfer')->group(function () {
+    Route::post('/', [TransferController::class, 'store']);
+    Route::get('/{transfer}', [TransferController::class, 'detail']);
+    Route::post('/confirmation/{transfer}', [TransferController::class, 'confirmation']);
   });
 });
