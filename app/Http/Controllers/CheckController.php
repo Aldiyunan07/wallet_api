dcbb5a053b9ed9c8fa7718fbfa03bca3b3ea1d25
aldiyunan07 <aldiyunan07@gmail.com>
2024-09-28 14:16:47 +0700

    update api scramble

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -14,6 +14,8 @@ class CheckController extends Controller
 
     public function __invoke(Request $request)
     {
-        return $request->user()->name;
+        return response()->json([
+            'data' => $request->user()->name
+        ]);
     }
 }
